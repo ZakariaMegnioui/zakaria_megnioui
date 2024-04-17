4bcf2ce108023811521d3183360896218be1c081
ZakariaMegnioui <fromvaliiri@gmail.com>
2024-04-17 16:40:43 +0100

    add the getters and setters

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -4,26 +4,79 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public const TABLE_NAME = 'categories';
+    public const PRIMARY_KEY_COLUMN = 'id';
+    public const NAME_COLUMN = 'name';
+    public const PARENT_ID_COLUMN = 'parent_id';
+    public const CREATED_AT_COLUMN = 'created_at';
+    public const UPDATED_AT_COLUMN = 'updated_at';
+
     protected $guarded = [];
 
-    public function category()
+  
+    public function subcategories(): HasMany
     {
-        return $this->belongsTo(Category::class, 'parent_id');
+        return $this->hasMany(Category::class, self::PARENT_ID_COLUMN);
     }
 
-    public function subcategories()
-    {
-        return $this->hasMany(Category::class, 'parent_id');
-    }
-
-   
     public function products(): BelongsToMany
     {
-        return $this->belongsToMany(Product::class, CategoryProduct::TABLE_NAME, CategoryProduct::PRODUCT_ID_COLUMN, CategoryProduct::CATEGORY_ID_COLUMN);
+        return $this->belongsToMany(Product::class, CategoryProduct::TABLE_NAME, CategoryProduct::CATEGORY_ID_COLUMN, CategoryProduct::PRODUCT_ID_COLUMN);
     }
+
+    /**
+     * Getters
+     */
+    public function getName(): string
+    {
+        return $this->getAttribute(self::NAME_COLUMN);
+    }
+
+    public function getParentId(): ?int
+    {
+        return $this->getAttribute(self::PARENT_ID_COLUMN);
+    }
+
+    public function getCreatedAt(): ?string
+    {
+        return $this->getAttribute(self::CREATED_AT_COLUMN);
+    }
+
+    public function getUpdatedAt(): ?string
+    {
+        return $this->getAttribute(self::UPDATED_AT_COLUMN);
+    }
+
+    /**
+     * Setters
+     */
+    public function setName(string $name): self
+    {
+        $this->setAttribute(self::NAME_COLUMN, $name);
+        return $this;
+    }
+
+    public function setParentId(?int $parentId): self
+    {
+        $this->setAttribute(self::PARENT_ID_COLUMN, $parentId);
+        return $this;
+    }
+
+      /**
+     * Relationships
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, self::PARENT_ID_COLUMN);
+    }
+
+
 }
--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,23 +2,72 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
-    use HasFactory;
     public const TABLE_NAME = 'products';
     public const PRIMARY_KEY_COLUMN = 'id';
     public const NAME_COLUMN = 'name';
     public const PRICE_COLUMN = 'price';
     public const DESCRIPTION_COLUMN = 'description';
+    public const IMAGE_COLUMN = 'image'; 
 
     protected $guarded = [];
+    /**
+     * Getters
+     */
+    public function getName(): string
+    {
+        return $this->getAttribute(self::NAME_COLUMN);
+    }
 
-  
+    public function getPrice(): float
+    {
+        return $this->getAttribute(self::PRICE_COLUMN);
+    }
+
+    public function getDescription(): string
+    {
+        return $this->getAttribute(self::DESCRIPTION_COLUMN);
+    }
+
+    public function getImage(): string
+    {
+        return $this->getAttribute(self::IMAGE_COLUMN);
+    }
+
+    /**
+     * Setters
+     */
+    public function setName(string $name): self
+    {
+        $this->setAttribute(self::NAME_COLUMN, $name);
+        return $this;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->setAttribute(self::PRICE_COLUMN, $price);
+        return $this;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->setAttribute(self::DESCRIPTION_COLUMN, $description);
+        return $this;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->setAttribute(self::IMAGE_COLUMN, $image);
+        return $this;
+    }
+
+     /**
+     * Relationships
+     */
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class, CategoryProduct::TABLE_NAME, CategoryProduct::PRODUCT_ID_COLUMN, CategoryProduct::CATEGORY_ID_COLUMN);
