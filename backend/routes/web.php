da94302883ea524fc8f9704f6432d9ace8503c3d
ZakariaMegnioui <fromvaliiri@gmail.com>
2024-04-17 19:11:17 +0100

    define the apis

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::apiResource('/products', ProductController::class);
+
