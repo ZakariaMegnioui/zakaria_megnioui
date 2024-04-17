3555f72f526156b4fbfd52a3952297fd4d122743
ZakariaMegnioui <fromvaliiri@gmail.com>
2024-04-17 16:28:32 +0100

    correct name of description col

--- backend/database/migrations/2024_04_17_144752_create_products_table.php
+++ backend/database/migrations/2024_04_17_144752_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('desciption');
+            $table->string('description');
             $table->decimal('price');
             $table->string('image');
             $table->timestamps();
