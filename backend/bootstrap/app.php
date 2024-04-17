5112328587db11859c6e47564cdcb09341dffbf0
ZakariaMegnioui <fromvaliiri@gmail.com>
2024-04-17 19:10:40 +0100

    disabled the cros

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'http://127.0.0.1:8000/products',
+            'http://127.0.0.1:8000/categories',
+            'http://127.0.0.1:8000/products/*',
+            'http://127.0.0.1:8000/categories/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
