06ce7abfbce81485b59b1e8675c8d2b83853a1bd
ZakariaMegnioui <fromvaliiri@gmail.com>
2024-04-17 16:29:03 +0100

    define relations in the new pivot  model

---
+++ backend/app/Models/CategoryProduct.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CategoryProduct extends Pivot
+{
+    use HasFactory;
+    public const PRIMARY_KEY_COLUMN = 'id';
+    public const PRODUCT_ID_COLUMN = 'product_id';
+    public const CATEGORY_ID_COLUMN = 'category_id';
+    public const CREATED_AT_COLUMN = 'created_at';
+    public const UPDATED_AT_COLUMN = 'updated_at';
+
+    protected $guarded = [];
+    
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, self::PRODUCT_ID_COLUMN);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, self::CATEGORY_ID_COLUMN);
+    }
+}
